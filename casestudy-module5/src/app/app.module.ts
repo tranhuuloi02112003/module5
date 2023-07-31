461fdc950d232b75bb0823e39644228ff19517a9
tranhuuloi02112003 <tranhuuloi2k3@gmail.com>
2023-07-31 17:52:39 +0700

    up

--- casestudy-module5/src/app/app.module.ts
+++ casestudy-module5/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ListContractComponent } from './list-contract/list-contract.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ListContractComponent
   ],
   imports: [
     BrowserModule,
